c664649c885fc038c2607180bdc9a631aa2cc911
Rayanderson Sousa <rayandersoncooper@gmail.com>
2022-08-23 16:36:40 -0300

    Requisito 3

---
+++ src/components/Card.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage,
+      cardRare, cardTrunfo } = this.props;
+
+    return (
+      <section>
+        <p data-testid="name-card">
+          { cardName }
+        </p>
+        <img
+          src={ cardImage }
+          alt={ cardName }
+          data-testid="image-card"
+        />
+        <p data-testid="description-card">
+          { cardDescription }
+        </p>
+        <p data-testid="attr1-card">
+          { cardAttr1 }
+        </p>
+        <p data-testid="attr2-card">
+          { cardAttr2 }
+        </p>
+        <p data-testid="attr3-card">
+          { cardAttr3 }
+        </p>
+        <p data-testid="rare-card">
+          { cardRare }
+        </p>
+        <div>
+          { cardTrunfo ? <span data-testid="trunfo-card">Super Trunfo</span> : '' }
+        </div>
+      </section>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropTypes.string,
+  cardDescription: PropTypes.string,
+  cardAttr1: PropTypes.string,
+  cardAttr2: PropTypes.string,
+  cardAttr3: PropTypes.string,
+  cardImage: PropTypes.string,
+  cardRare: PropTypes.string,
+  cardTrunfo: PropTypes.bool,
+}.isRequired;
+
+export default Card;
