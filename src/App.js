6c7ee6cf943880918c63f2850dd81ef9010b98ff
Rayanderson Sousa <rayandersoncooper@gmail.com>
2022-08-24 19:16:17 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -21,9 +21,30 @@ class App extends React.Component {
     this.setState({ [name]: valor });
   };
 
+  attributes = (attr1, attr2, attr3) => {
+    const firstA = parseInt(attr1, 10);
+    const secondA = parseInt(attr2, 10);
+    const thirdA = parseInt(attr3, 10);
+    const maxAttr = 90;
+    const trueOrFalseA1 = (firstA <= maxAttr && firstA >= 0);
+    const trueOrFalseA2 = (secondA <= maxAttr && secondA >= 0);
+    const trueOrFalseA3 = (thirdA <= maxAttr && thirdA >= 0);
+    const maxSum = 210;
+    const totalAttTrue = (firstA + secondA + thirdA <= maxSum);
+    return (trueOrFalseA1 && trueOrFalseA2 && trueOrFalseA3 && totalAttTrue);
+  };
+
+  formIsFilled = (rare, description, name, image) => (
+    rare !== '' && description !== '' && name !== '' && image !== '');
+
+  saveButton = (param1, param2) => param1 && param2;
+
   render() {
     const { cardAttr1, cardAttr2, cardAttr3, cardDescription, cardImage,
       cardName, cardRare, cardTrunfo } = this.state;
+
+    const formTrue = this.formIsFilled(cardRare, cardDescription, cardName, cardImage);
+    const attributeTrue = this.attributes(cardAttr1, cardAttr2, cardAttr3);
 
     return (
       <div>
@@ -38,6 +59,7 @@ class App extends React.Component {
           cardImage={ cardImage }
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
+          isSaveButtonDisabled={ !this.saveButton(formTrue, attributeTrue) }
         />
         <Card
           cardName={ cardName }
